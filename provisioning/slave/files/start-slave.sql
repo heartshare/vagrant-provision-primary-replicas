f0d850ae8578984e8579e7bbbd0d5cfd64942b97
GOTO Satoru <satoru.goto@mariadb.com>
2018-02-22 12:04:23 +0900

    repl useer privileges:   REPLICATION CLIENT, REPLICATION SLAVE

--- provisioning/slave/files/start-slave.sql
+++ provisioning/slave/files/start-slave.sql
@@ -1,6 +1,6 @@
 stop slave;
 reset slave;
-set global gtid_slave_pos = "0-11-937";
+set global gtid_slave_pos = "0-11-6";
 change master to master_host='192.168.100.11',
   master_user='repl',
   master_password='replpwd',
