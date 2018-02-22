14a2ec5eea6f97464ac414622fd3083e67506c27
GOTO Satoru <satoru.goto@mariadb.com>
2018-02-22 09:28:10 +0900

    updated variables

---
+++ provisioning/maxscale/files/sysbench.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+usage() {
+  echo "Usage: ./sysbench.sh <prepare|run|cleanup> <num of threads> <read_write|read_only>"
+  exit 1
+}
+
+echo $1 $2 $3
+host=127.0.0.1
+port=4006
+lua=/usr/share/sysbench/oltp_$3.lua
+size=1000000
+
+if [ "$1" = "" -o $# -gt 4 ]; then
+  usage 1
+elif [ "$2" = "" ]; then
+  nthreads=1
+else
+  nthreads=$2
+fi
+
+sysbench --db-driver=mysql --mysql-host=$host --mysql-port=$port \
+  --mysql-user=maxuser --mysql-password=maxpwd --mysql-db=test \
+  --range_size=100 --table_size=$size --tables=2 --events=0 \
+  --time=60 --rand-type=uniform --db-ps-mode=disable --threads=$nthreads $lua $1
