770fbc1b7d4c619313045d4207067291568fb547
GOTO Satoru <satoru.goto@mariadb.com>
2018-02-23 14:55:47 +0900

    update for MXS 2.2

--- provisioning/slave/files/start-slave.sql
+++ provisioning/slave/files/start-slave.sql
@@ -1,7 +1,7 @@
 stop slave;
 reset slave;
 set global gtid_slave_pos = "0-11-6";
-change master to master_host='192.168.100.11',
+change master to master_host='192.168.2.11',
   master_user='repl',
   master_password='replpwd',
   master_use_gtid=slave_pos;
