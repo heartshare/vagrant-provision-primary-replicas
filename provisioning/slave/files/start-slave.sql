03df859371291bcc3079067071e70bde576caebc
GOTO Satoru <satoru.goto@mariadb.com>
2018-04-03 11:44:37 +0900

    updated   maxscale.cnf   start-slave.sql

--- provisioning/slave/files/start-slave.sql
+++ provisioning/slave/files/start-slave.sql
@@ -1,6 +1,6 @@
 stop slave;
 reset slave;
-set global gtid_slave_pos = "0-101-6";
+set global gtid_slave_pos = '0-101-6';
 change master to master_host='192.168.2.101',
   master_user='repl',
   master_password='replpwd',
