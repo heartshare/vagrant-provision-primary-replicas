985d482229717af8df9cb8baf44d47b43a54659d
GOTO Satoru <goto.satoru@gmail.com>
2019-06-30 08:58:21 +0900

    rename start-slave.sql

---
+++ provisioning/replica/files/start-replica.sql
@@ -0,0 +1,8 @@
+STOP SLAVE;
+RESET SLAVE;
+set global gtid_slave_pos = '0-101-6';
+CHANGE MASTER TO master_host='192.168.2.101',
+  master_user='repl',
+  master_password='replpwd',
+  master_use_gtid=slave_pos;
+START SLAVE;
