0f0e8717c0f811c81fb3dc90b3de87ee85600dfd
GOTO Satoru <goto.satoru@gmail.com>
2020-01-30 19:33:42 +0900

    test passed

--- provisioning/maxscale/files/is_maxscale_running.sh
+++ provisioning/maxscale/files/is_maxscale_running.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
-fileName="maxctrl_output.txt"
-rm $fileName
-timeout 5s maxctrl list servers > $fileName
+file_name="/tmp/maxctrl_list_servers.txt"
+rm $file_name
+timeout 5s maxctrl list servers > $file_name
 to_result=$?
 if [ $to_result -ge 1 ]
 then
   echo Timed out or error, timeout returned $to_result
   exit 3
 else
-  echo MaxAdmin success, rval is $to_result
+  echo maxctrl success, rval is $to_result
   echo Checking maxctrl output sanity
-  grep1=$(grep server1 $fileName)
-  grep2=$(grep server2 $fileName)
+  grep1=$(grep server1 $file_name)
+  grep2=$(grep server2 $file_name)
 
   if [ "$grep1" ] && [ "$grep2" ]
   then
-    echo All is fine
+    echo Everything is fine
     exit 0
   else
     echo Something is wrong
